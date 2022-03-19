101964c423d2ac99e7a3afdfd7a86d4f40ad9696
Nadav Zingerman <7372858+nzig@users.noreply.github.com>
2022-03-19 23:49:28 +0200

    Add pick mode

--- functions/_fzf_configure_bindings_help.fish
+++ functions/_fzf_configure_bindings_help.fish
@@ -15,6 +15,7 @@ DESCRIPTION
         Search variables     |  Ctrl+V     (V for variable)  |  --variables
         Search processes     |  Ctrl+Alt+P (P for process)   |  --processes
 	    Magic mode           |  Alt+M      (M for magic)     |  --magic
+	    Pick mode            |  Alt+X                        |  --pick
     An option with a key sequence value overrides the binding for its feature, while an option
     without a value disables the binding. A feature that is not customized retains its default
     mnemonic binding specified above. Key bindings are installed for default and insert modes.
